2224358e90bad7e8bca0e439ea9fc89f541735af
jlbarredo <jlbarredo@users.noreply.github.com>
2018-03-31 16:20:38 -0400

    Update batch_behavior.m
    Now collates behavioral stats for each run.

--- batch_behavior.m
+++ batch_behavior.m
@@ -2,10 +2,15 @@
 %  for later fMRI or behavioral analyses. This script and the function that
 %  loads and sorts the onset need to be in the script folder.
 
-studyfolder = '/Volumes/Luria/CONTE_OCD';
-scriptfolder = '/Volumes/Luria/CONTE_OCD/scripts';
-subjectlist = '/Volumes/Luria/CONTE_OCD/scripts/subjects.txt';
+studyfolder = '/Users/jenniferbarredo/Desktop/CONTE_OCD';
+scriptfolder = '/Users/jenniferbarredo/Desktop/CONTE_OCD/scripts';
+subjectlist = '/Users/jenniferbarredo/Desktop/CONTE_OCD/scripts/subjects.txt';
+
+addpath(genpath(scriptfolder));
 subjects = num2cell(dlmread(subjectlist))';
+
+%% Blockwise statistics and onsets
+stats = zeros(length(subjects),8);
 
 for i = 1:length(subjects)
     subject = num2str(subjects{i})
@@ -13,12 +18,14 @@ for i = 1:length(subjects)
         if run==1
             outfolder = fullfile(studyfolder,subject,'BL_MRI');
             filename = fullfile(studyfolder,'SSRT-behavior',[subject,'_BL_1.xlsx']);
-            load_ssrt_xlsx(filename,outfolder,run)
+            [stats(i,1),stats(i,2),stats(i,3),stats(i,4)] = load_ssrt_xlsx(filename,outfolder,run);
         elseif run==2 
             outfolder = fullfile(studyfolder,subject,'BL_MRI');
             filename = fullfile(studyfolder,'SSRT-behavior',[subject,'_BL_2.xlsx']);
-            load_ssrt_xlsx(filename,outfolder,run)
+            [stats(i,5),stats(i,6),stats(i,7),stats(i,8)] = load_ssrt_xlsx(filename,outfolder,run);
         end
         
     end
 end
+
+dlmwrite([studyfolder,'/groupstats.txt'],stats,'delimiter','\t')
