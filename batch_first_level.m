888f75c055a83728d03c88bdc1b79fb2b6416ef2
jlbarredo <jlbarredo@users.noreply.github.com>
2018-04-02 19:04:47 -0400

    Update batch_first_level.m

--- batch_first_level.m
+++ batch_first_level.m
@@ -1,18 +1,18 @@
 % List of open inputs
-addpath(genpath('~/Documents/MATLAB/spm_utilities'))
+addpath(genpath('/Volumes/Luria/CONTE_OCD/scripts'))
 directory = ('/Volumes/Luria/CONTE_OCD');
 %sessions = {'BL_MRI','Post-tx_MRI'};       % For patients
 sessions = {'BL_MRI'};
-analysis = 'MSIT_BK_010918';
+analysis = 'SSRT_040218';
 bpath = [directory,'/MSIT_SSRT-EPRIME/Controls'];  % Path to behavioral files
-subjects = num2cell(dlmread('/Volumes/Luria/CONTE_OCD/scripts/con_msit_subjects.txt'))';
+subjects = num2cell(dlmread('/Volumes/Luria/CONTE_OCD/scripts/subjects.txt'))';
 %subjects = {'101'};
 
 %% Task-specific modifiers
 slices = 64;
 refslice = 1;
 TR = 1.0;
-func = 'swumsit.nii';
+func = 'swussrt.nii';
 highpass = 128;     % 2 times the longest task event cycle or default 128
 
 for i=1:length(subjects)
@@ -21,19 +21,19 @@ for i=1:length(subjects)
     
     subfile=[directory,'/',subject];
     cd(subfile);
-    mkdir MSIT_BK_010918;
+    mkdir SSRT_040218;
     
     for j=1:length(sessions)
         cd(fullfile(subfile,sessions{j}));
-        files = fullfile(directory,subject,sessions{j},'swumsit.nii');              % Functional vols
-        mask = fullfile(directory,subject,sessions{j},'c3art_mean_umsit.nii');      % CSF mask
+        files = fullfile(directory,subject,sessions{j},['swussrt',num2str(j),'.nii']);              % Functional vols
+        mask = fullfile(directory,subject,sessions{j},'c3art_mean_ssrt.nii');      % CSF mask
         
         [csf] = getTC(files,mask);                                                  % Extracts CSF tc - make sure function is on path
         save(['csf',num2str(j),'.mat'],'csf');
     end
      
     cd([directory,'/scripts']);
-    msit_block_con;                                                                     % Analysis script
+    ssrt_patients;                                                                     % Analysis script
     
     spm_jobman('initcfg')
     spm('defaults', 'FMRI');
